bc5be0ebf1cd54430521284fff1c622e54c0bba8
Vincenzo Jr Colabelli <vincenjr98@icloud.com>
2024-04-03 19:46:55 +0200

    esercitazione conclusa

---
+++ Esercitazione9_Traccia2_03-04-2024/js/main.js
@@ -0,0 +1,20 @@
+alert("Ciao Mondo")
+
+console.log("Ciao Mondo")
+
+let num1 = 10;
+let num2 = 15;
+
+console.log("Il valore della variabile num1 e': " + num1);
+console.log("Il valore della variabile num2 e': " + num2);
+
+let stringa;
+stringa = 40;
+
+console.log("stringa")
+
+console.log("Il valore della variabile stringa e' " + stringa);
+
+const PIGRECO ="30";
+console.log("Il valore di PIGRECO e': " + PIGRECO)
+
