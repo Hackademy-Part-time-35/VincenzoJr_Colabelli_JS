27afbd9ecb6e458c9e16e77331188af66e71a84e
Vincenzo Jr Colabelli <vincenjr98@icloud.com>
2024-04-07 16:56:20 +0200

    Esercitazione completata

---
+++ Esercitazione10_05-04-2024/Traccia2/main.js
@@ -0,0 +1,11 @@
+let current_year = 2024;
+let birth_year = 1972;
+
+let years_old = current_year - birth_year;
+let years_left = 100 - years_old;
+
+console.log(`Hai ${years_old} e ti mancano ${years_left} per compierne 100`);
+
+
+
+
---
+++ Esercitazione10_05-04-2024/Traccia3/main.js
@@ -0,0 +1,11 @@
+let variabile1 = prompt("Inserisci la prima variabile");
+let variabile2 = prompt("Inserisci la seconda variabile");
+
+let somma = parseInt(variabile1) + parseInt(variabile2);
+let sottrazione = variabile1 - variabile2;
+let moltiplicazione = variabile1 * variabile2;
+let divisione = variabile1 / variabile2;
+let potenza = variabile1 ** variabile2;
+let modulo = variabile1 % variabile2;
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma = ${somma}, sottrazione = ${sottrazione}, moltiplicazione = ${moltiplicazione}, divisione = ${divisione}, potenza = ${potenza}, modulo ${modulo}`);
