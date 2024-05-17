8f37f2c5221cbd88498b1426ffa83cb9cfa98486
Vincenzo Jr Colabelli <vincenjr98@icloud.com>
2024-05-17 13:10:05 +0200

    Filtri citta aggiunti

--- Progetto_Presto.it/js/annunci.js
+++ Progetto_Presto.it/js/annunci.js
@@ -1,12 +1,18 @@
 let cont_annunci = document.querySelector('#cont_annunci');
 
+let filtra_city = document.querySelector('#filtra_city');
+
+let filtra_prezzo = document.querySelector('#filtra_prezzo');
+
 fetch('./dati/elenco_immobili.json')
     .then(response => response.json())
     .then(data =>{
 
-        function creaCardAnnunci() {
+        function creaCardAnnunci(array_annunci) {
+
+            cont_annunci.innerHTML = '';
             
-            data.forEach(annuncio => {
+            array_annunci.forEach(annuncio => {
                 
                 let colonna = document.createElement('div');
                 
@@ -27,6 +33,69 @@ fetch('./dati/elenco_immobili.json')
             });
         }
 
-        creaCardAnnunci();
+        creaCardAnnunci(data);
+
+
+
+        function creaRadioAnnunci() {
+
+            let elencoUnicheCity = [];
+
+            data.forEach((annuncio) => {
+
+                if (!elencoUnicheCity.includes(annuncio.city)){
+                    elencoUnicheCity.push(annuncio.city)
+                }
+            })
+
+            elencoUnicheCity.forEach((singola_city) =>{
+
+                let div = document.createElement('div');
+                div.classList.add('form-check');
+
+                div.innerHTML = `
+                <input checked class="form-check-input" type="radio" name="sel_citta" id="${singola_city}">
+                <label class="form-check-label" for="${singola_city}">${singola_city}
+                </label>
+                
+                `;
+
+                filtra_city.appendChild(div);
+
+            })
+            
+        }
+
+        creaRadioAnnunci();
+
+        function filtraCity(city) {
+
+            if (city == 'all') {
+                creaCardAnnunci(data)
+            } else {
+                let annunciFiltrati = data.filter(annuncio => annuncio.city == city);
+                creaCardAnnunci(annunciFiltrati);
+            }
+            
+        }
+
+        let radioCity = document.querySelectorAll('.form-check-input');
+
+        radioCity.forEach(singola_radio =>{
+
+            singola_radio.addEventListener('click', () => {
+                let city_selezionata = singola_radio.id;
+
+                filtraCity(city_selezionata);
+            })
+        })
+
+
 
     });
+
+
+
+        
+
+ 
